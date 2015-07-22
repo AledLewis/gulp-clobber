767c0e6e3c775681a3d44623929a98a027f0f2f1
Aled Lewis <github@aled-lewis.co.uk>
2015-07-22 22:54:58 +0100

    Make ScriptRunner location configurable

--- index.js
+++ index.js
@@ -11,11 +11,13 @@ var gutil = require('gulp-util');
 var fs = require('fs-extra')
 var uuid = require('uuid');
 var format = require('string-format');
+var temp = require('temp');
+
 format.extend(String.prototype);
 
 var clobber_dir_name = '.clobber';
-var cmd_sr_build = 'java -jar ScriptRunner.jar -build {0} -label {1} -outfile {2} -logdir {3}';
-var cmd_sr_run = 'java -jar ScriptRunner.jar -run {0} -jdbc {1} -user {2} -password {3} -logdir {4}';
+var cmd_sr_build = 'java -jar "{0}" -build {1} -label {2} -outfile {3} -logdir {4}';
+var cmd_sr_run = 'java -jar "{0}" -run {1} -jdbc {2} -user {3} -password {4} -logdir {5}';
 
 function logSuccess(message) {
   gutil.log(chalk.green('Success: ') + message);
@@ -52,36 +54,34 @@ module.exports = function(changed_file) {
   });
   file.name = file.path.replace(/^.*[\\\/]/, '');
   file.dirnamerelative = file.relative.replace(path.sep + file.name, '');
-
-  var target_working_dir = path.join(clobber_dir_name, runid);
-  var target_build_dir = path.join(target_working_dir, 'build');
-
-  fs.mkdirsSync(path.join(target_build_dir, file.dirnamerelative));
-  fs.copySync(file.relative, path.join(target_build_dir, file.relative));
-  fs.copySync('ScriptRunner', path.join(target_build_dir, 'ScriptRunner'));
-  fs.emptyDirSync(path.join(target_build_dir, 'ScriptRunner', 'Utils'));
-  fs.rmdirSync(path.join(target_build_dir, 'ScriptRunner', 'Utils'));
-  fs.emptyDirSync(path.join(target_build_dir, 'ScriptRunner', 'Jobs'));
-  fs.rmdirSync(path.join(target_build_dir, 'ScriptRunner', 'Jobs'));
-
-  var build_label = 'clobber-{0}-{1}'.format(os.hostname(), runid);
-  var build_cmd = cmd_sr_build.format(target_build_dir, build_label, path.join(target_working_dir, build_label + '.zip'), target_working_dir);
-  var run_cmd = cmd_sr_run.format(path.join(target_working_dir, build_label + '.zip'), config.scriptrunner.jdbc, config.scriptrunner.user, config.scriptrunner.password, target_working_dir);
-
-  exec(build_cmd, function(err) {
-    if (err) {
-      logFailure(file.relative + ' was not clobbed.' + err);
-      return;
-    }
-
-    exec(run_cmd, function(err) {
+  temp.mkdir(clobber_dir_name, function(err, target_working_dir){
+    if (err) throw err;
+    var target_build_dir = path.join(target_working_dir, 'build');
+    fs.mkdirsSync(path.join(target_build_dir, file.dirnamerelative));
+    fs.copySync(file.relative, path.join(target_build_dir, file.relative));
+    fs.copySync('ScriptRunner', path.join(target_build_dir, 'ScriptRunner'));
+    fs.emptyDirSync(path.join(target_build_dir, 'ScriptRunner', 'Utils'));
+    fs.rmdirSync(path.join(target_build_dir, 'ScriptRunner', 'Utils'));
+    fs.emptyDirSync(path.join(target_build_dir, 'ScriptRunner', 'Jobs'));
+    fs.rmdirSync(path.join(target_build_dir, 'ScriptRunner', 'Jobs'));
+    var build_label = 'clobber-{0}-{1}'.format(os.hostname(), runid);
+    var build_cmd = cmd_sr_build.format(config.scriptrunner.jarLocation, target_build_dir, build_label, path.join(target_working_dir, build_label + '.zip'), target_working_dir);
+    var run_cmd = cmd_sr_run.format(config.scriptrunner.jarLocation,path.join(target_working_dir, build_label + '.zip'), config.scriptrunner.jdbc, config.scriptrunner.user, config.scriptrunner.password, target_working_dir);
+    exec(build_cmd, function(err) {
       if (err) {
-        logFailure(file.relative + ' was not clobbed. Check ScriptRunner log in ' + target_working_dir + ' for more details. ' + err);
+        logFailure(file.relative + ' was not clobbed.' + err);
         return;
       }
-      fs.emptyDirSync(target_working_dir);
-      fs.rmdirSync(target_working_dir);
-      logSuccess(file.relative + ' was clobbed.');
+      exec(run_cmd, function(err) {
+        if (err) {
+          logFailure(file.relative + ' was not clobbed. Check ScriptRunner log in ' + target_working_dir + ' for more details. ' + err);
+          return;
+        }
+        fs.emptyDirSync(target_working_dir);
+        fs.rmdirSync(target_working_dir);
+        logSuccess(file.relative + ' was clobbed. Build at:'+target_working_dir);
+      });
     });
   });
+
 };
